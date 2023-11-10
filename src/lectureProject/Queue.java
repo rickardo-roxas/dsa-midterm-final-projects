257b8987345b72fb5b23ecda50be88ef77c4ab92
Karl Jasper Urbiztondo <2233067@slu.edu.ph>
2023-11-10 22:37:29 +0800

    Added code for Queue

--- src/lectureProject/Queue.java
+++ src/lectureProject/Queue.java
@@ -1,7 +1,35 @@
 package lectureProject;
+import Project1.backend.stack.Node;
 
 /**
  * Author: Karl Jasper Urbiztondo
  */
-public class Queue {
+public class Queue<T> {
+    int size;
+    private Node<T> head;
+    private Node<T> tail;
+
+    public Queue() {
+        head = tail = new Node<>(null);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T firstElement() {
+        if (!isEmpty() && head.getNext() != null)
+            return head.getNext().getData();
+        else return null;
+    }
+
+    public T lastElement() {
+        if (!isEmpty())
+            return tail.getData();
+        else return null;
+    }
 }
