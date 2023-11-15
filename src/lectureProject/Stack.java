eacaa9bec1e2fb4c7f58c2cce34f3fad1411daa8
Rithik Tank <2233293@slu.edu.ph>
2023-11-15 21:43:12 +0800

    Made node class and fixed stack class

--- src/lectureProject/Stack.java
+++ src/lectureProject/Stack.java
@@ -8,52 +8,54 @@
  */
 
 package lectureProject;
+import Project1.backend.stack.Node;
+
 import java.util.EmptyStackException;
 
 public class Stack<T> {
-    private int capacity;
-    private Object[] elements;
-    private int top;
+    private int count;
+    private Node<T> top;
 
-    public Stack(int capacity) {
-        this.capacity = capacity;
-        this.elements = new Object[capacity];
-        this.top = -1;
+    public Stack() {
+        count = 0;
+        this.top = null;
     }
 
     public void push(T item) {
-        if (top < capacity - 1) {
-            elements[++top] = item;
-        } else {
-            throw new StackOverflowError("Stack is full");
-        }
+        Project1.backend.stack.Node<T> node = new Node<>(item);
+        if (!isEmpty())
+            node.setNext(top);
+
+        top = node;
+        count++;
     }
 
     public T pop() {
+        T topElement = null;
         if (!isEmpty()) {
-            T item = (T) elements[top];
-            elements[top] = null;
-            top--;
-            return item;
-        } else {
-            throw new EmptyStackException();
-        }
+            topElement = top.getData();
+            if (count == 1)
+                top = null;
+            else
+                top = top.getNext();
+            count--;
+        } // end of if
+        return topElement;
     }
 
     public T peek() {
         if (!isEmpty()) {
-            return (T) elements[top];
-        } else {
-            throw new EmptyStackException();
-        }
+            return top.getData(); // returns if stack and top is not null
+        } // end of if-else (isEmpty)
+        return null;
     }
 
     public boolean isEmpty() {
-        return top == -1;
+        return size() == 0;
     }
 
     public int size() {
-        return top + 1;
+        return this.count;
     }
 }
 
