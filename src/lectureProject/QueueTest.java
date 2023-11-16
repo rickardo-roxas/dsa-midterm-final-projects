62c7d5a02638a8b2d79bf6e249922eeb885ff00d
Rithik Tank <2233293@slu.edu.ph>
2023-11-16 18:25:10 +0800

    Fixed Queue

--- src/lectureProject/QueueTest.java
+++ src/lectureProject/QueueTest.java
@@ -2,6 +2,20 @@ package lectureProject;
 
 public class QueueTest {
     public static void main(String[] args) {
+        Queue<String> bankLine = new Queue<String>();
+        bankLine.enqueue("Karl Jasper");
+        bankLine.peek();
+        bankLine.enqueue("Rithik");
+        bankLine.enqueue("Patrick");
+        bankLine.enqueue("Johan");
+        bankLine.enqueue("Ramon");
+        System.out.println(bankLine.peek());
+        bankLine.dequeue();
+        System.out.println(bankLine.peek());
+        bankLine.dequeue();
+        System.out.println(bankLine.peek());
+
+
 
     }
 }
