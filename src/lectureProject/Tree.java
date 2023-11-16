3fe220d95ce0908ab8e767d096153736e801097a
Sebastian <2233205@slu.edu.ph>
2023-11-16 21:49:48 +0800

    added TreeTest runnable

--- src/lectureProject/Tree.java
+++ src/lectureProject/Tree.java
@@ -7,32 +7,41 @@ class Tree<T> {
     T data;
     Tree<T> left;
     Tree<T> right;
+
+    // Constructor to initialize a Tree node with given data
     public Tree(T data){
         this.data = data;
         this.left = null;
         this.right = null;
     }
+
+    // Getter method to retrieve the data stored in the tree node
     public T getData() {
         return data;
     }
 
+    // Setter method to set the data in the tree node
     public void setData(T data) {
         this.data = data;
     }
 
-    public Tree getLeft() {
+    // Getter method to retrieve the left child of the current tree node
+    public Tree<T> getLeft() {
         return left;
     }
 
-    public void setLeft(Tree left) {
+    // Setter method to set the left child of the current tree node
+    public void setLeft(Tree<T> left) {
         this.left = left;
     }
 
-    public Tree getRight() {
+    // Getter method to retrieve the right child of the current tree node
+    public Tree<T> getRight() {
         return right;
     }
 
-    public void setRight(Tree right) {
+    // Setter method to set the right child of the current tree node
+    public void setRight(Tree<T> right) {
         this.right = right;
     }
 }
--- src/lectureProject/TreeTest.java
+++ src/lectureProject/TreeTest.java
@@ -1,8 +1,51 @@
 package lectureProject;
-
 public class TreeTest {
 
+    /**
+     * Main method to test the Tree class functionality.
+     * @param args Command line arguments (not used in this example).
+     */
     public static void main(String[] args) {
+        Tree<Integer> root = new Tree<>(5);
+        root.setLeft(new Tree<>(2));
+        root.setRight(new Tree<>(6));
+        root.getLeft().setLeft(new Tree<>(8));
+        root.getLeft().setRight(new Tree<>(1));
 
+        System.out.println("Tree Structure:");
+        displayTree(root, 0);
+
+        System.out.println("\nTree Data:");
+        printTreeData(root);
+    }
+
+    /**
+     * A method to display the tree structure using recursion.
+     *
+     * @param node  The current tree node being processed.
+     * @param level The level of the current tree node in the tree structure.
+     */
+    private static void displayTree(Tree<?> node, int level) {
+        if (node != null) {
+            displayTree(node.getRight(), level + 1);
+            for (int i = 0; i < level; i++) {
+                System.out.print("    ");
+            }
+            System.out.println(node.getData());
+            displayTree(node.getLeft(), level + 1);
+        }
+    }
+
+    /**
+     * A method to print data from the tree nodes using recursion.
+     *
+     * @param node The current tree node being processed.
+     */
+    private static void printTreeData(Tree<?> node) {
+        if (node != null) {
+            printTreeData(node.getLeft());
+            System.out.print(node.getData() + " ");
+            printTreeData(node.getRight());
+        }
     }
 }
