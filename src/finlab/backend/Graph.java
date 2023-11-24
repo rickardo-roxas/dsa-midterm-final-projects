12daa7a4f2e193c4c43aea7061abf70491f3baec
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-11-25 04:45:40 +0800

    Added final draft for Node.java

--- src/finlab/backend/Graph.java
+++ src/finlab/backend/Graph.java
@@ -1,7 +1,21 @@
 package finlab.backend;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Graph {
-    private List<Node> nodes = new ArrayList<>()
+    private List<Node> nodes = new ArrayList<>();
+    private int count;
+
+    public Graph() {
+        nodes = null;
+        count = 0;
+    }
+
+    public Graph(List<Node> nodes, int count) {
+        this.nodes = nodes;
+        this.count = count;
+    }
+
+    se
 }
--- src/finlab/backend/Node.java
+++ src/finlab/backend/Node.java
@@ -1,6 +1,38 @@
 package finlab.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     private String label;
-    private
+    private List<Edge> adjacencyList = new ArrayList<>();
+
+    public Node() {
+        label = "";
+        adjacencyList = null;
+    }
+
+    public Node(String label) {
+        this.label = label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setAdjacencyList(List<Edge> adjacencyList) {
+        this.adjacencyList = adjacencyList;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<Edge> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public String toString() {
+        return label;
+    }
 }
